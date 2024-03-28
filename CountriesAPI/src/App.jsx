270cd5164d8e957b74edd2b92116b41cbc9a02d0
Ialbertine <a.ingabire1@alustudent.com>
2024-03-28 16:40:26 +0200

    react tailwind countries Apis

---
+++ CountriesAPI/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Components/Navbar";
+
+const App = () => {
+  return (
+     <>
+     <Navbar/>
+    <div className='bg-red-200 w-full h-screen'></div>
+    </>
+  );
+}
+
+export default App
---
+++ CountriesAPI/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    let links = []
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex bg-black py-4 ">
+        <div className="cursor-pointer">
+          <img src="./image/logo.svg" alt="logo" />
+        </div>
+        <ul>
+          <li>Countries</li>
+          <li>Continents</li>
+          <li>Regions</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
