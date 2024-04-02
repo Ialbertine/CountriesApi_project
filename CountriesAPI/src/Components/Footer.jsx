ef75fe6977685fe567efbe631bc2e2f1182b53a8
Ialbertine <a.ingabire1@alustudent.com>
2024-04-02 14:37:37 +0200

    Countries APIs React with tailwind

--- CountriesAPI/src/Components/Footer.jsx
+++ CountriesAPI/src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex justify-between w-full">
+    <div className="flex justify-between w-full bg-gray-100">
       <img src="./image/logo.svg" alt="logo" className=" md:text-[20px]" />
       <p>
         Copy 2024. All rights reserved
